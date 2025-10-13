49c5cf2a58a07a13e92845862494dd42ef0b7445
Christian Vorhofer <christian.vor26@gmail.com>
2025-10-13 14:37:42 +0200

    feat(init): removed routes

--- src/routes/blueprint.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from abc import ABC, abstractmethod
-
-class BaseBlueprint(ABC):
-    def __init__(self, name, import_name, url_prefix=None):
-        self.blueprint = Blueprint(name, import_name, url_prefix=url_prefix)
